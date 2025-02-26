8651e0d8907a898a2a1251be76d4aeaf6ffd4be4
Yonatan Amir <jonathanamir1@gmail.com>
2025-02-26 10:46:34 +0100

    hero links established

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-"use client"; // Required to use usePathname()
-import React from "react";
-import { usePathname } from "next/navigation";
-import Navbar from "./_components/Navbar";
-import Footer from "./_components/Footer";
+'use client'; // Required to use usePathname()
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import Navbar from './_components/Navbar';
+import Footer from './_components/Footer';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname(); // Get the current route
-  const isNotFoundPage = pathname === "/not-found"; // Check if it's the 404 page
+	const pathname = usePathname(); // Get the current route
+	const isNotFoundPage = pathname === '/not-found'; // Check if it's the 404 page
 
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>YAmir Website</title>
-      </head>
-      <body className=' h-screen'>
-        {/*{!isNotFoundPage && <Navbar />}  Hide Navbar on 404 page */}
-        {children}
-        {/*{!isNotFoundPage && <Footer />}  Hide Footer on 404 page */}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='UTF-8' />
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<title>YAmir Website</title>
+			</head>
+			<body className=' h-screen'>
+				{/*{!isNotFoundPage && <Navbar />}  Hide Navbar on 404 page */}
+				{children}
+				{/*{!isNotFoundPage && <Footer />}  Hide Footer on 404 page */}
+			</body>
+		</html>
+	);
 }
